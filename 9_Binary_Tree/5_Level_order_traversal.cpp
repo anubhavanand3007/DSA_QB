d146dff99cc2d091898d14c083555269b11f51ff
Anubhav <anubhavanand3007@gmail.com>
2022-07-13 15:50:23 +0530

    level order traversal of tree

---
+++ 9_Binary_Tree/5_Level_order_traversal.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class tree{
+    public:    
+        int data;
+        tree* left;
+        tree* right;
+
+        tree(int data){
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+vector<int> level_order_traversal(tree* head){
+    queue<tree*> q;
+    q.push(head);
+    vector<int> levelorder;
+    while(!q.empty()){
+        if(q.front()->left)q.push(q.front()->left);
+        if(q.front()->right)q.push(q.front()->right);
+
+        levelorder.push_back(q.front()->data);
+        q.pop();
+    }
+    return levelorder;
+}
+
+int main(){
+    tree* head = new tree(1);
+    head->left = new tree(2);
+    head->right = new tree(3);
+    head->left->left = new tree(4);
+    head->left->left->left = new tree(5);
+    head->left->left->right = new tree(6);
+
+    vector<int> levelorder = level_order_traversal(head);
+    for(auto i:levelorder) cout << i << ' ';
+
+    return 0;
+}
