3beebdd2781de6dcae0c7061a2aea69cfaaf1921
Anubhav <anubhavanand3007@gmail.com>
2022-07-13 22:22:17 +0530

    height of a tree

---
+++ 9_Binary_Tree/16_Height_of_binary_tree.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class tree{
+    public:
+        int data;
+        tree* left;
+        tree* right;
+
+    tree(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int height_of_tree(tree* head, int &diameter, int &height){
+    if(head == NULL) {
+        height = 0;
+        return 0;
+    }
+
+    int lh = 0, rh = 0;
+
+    height_of_tree(head->left, diameter, lh);
+    height_of_tree(head->right, diameter, rh);
+
+    height = max(rh,lh)+1;
+}
+
+int main(){
+    tree* a = new tree(2);
+    a->left = new tree(2);
+    a->left->left = new tree(2);
+    a->right = new tree(2);
+
+    int height = 0;
+    height_of_tree(a,height,height);
+    cout << height;
+
+    return 0;
+}
