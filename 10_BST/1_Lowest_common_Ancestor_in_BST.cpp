656cdcad2b7395b45e2de3e969a13c0e89000b13
Anubhav <anubhavanand3007@gmail.com>
2022-07-16 12:46:07 +0530

    Lowest common ancestor in BST

---
+++ 10_BST/1_Lowest_common_Ancestor_in_BST.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class tree{
+    public:    
+        int data;
+        tree* left;
+        tree* right;
+
+        tree(int data){
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+int LCAinBST(tree* root, int n1, int n2){
+    if(root == NULL)return -1;
+
+    if( (n1 < root->data && n2 > root->data) || (n1 > root->data && n2 < root->data)) return root->data;
+    else if(n1 < root->data && n2 < root->data)LCAinBST(root->left, n1, n2);
+    else LCAinBST(root->right,n1,n2);
+}
+
+int main(){
+    tree* head = new tree(10);
+    head->left = new tree(20);
+    head->right = new tree(30);
+    head->left->left = new tree(1);
+    head->left->left->left = new tree(0);
+    head->left->left->right = new tree(6);
+
+    cout << LCAinBST(head,6,30);
+
+    return 0;
+}
